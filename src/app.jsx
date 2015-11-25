b0e982ad5320b34b0b69cb5ce7e51dabcb5591de
Ian Zhang <ian@gingrapp.com>
2015-11-24 18:17:01 -0700

    Exporting form elements too

--- src/app.jsx
+++ src/app.jsx
@@ -3,7 +3,8 @@ import 'babel-polyfill';
 import FormBuilder from './form-builder';
 import FormGenerator from './form-generator';
 import FormElement from './form-elements/util/form-element.jsx';
+import * as FormElements from './form-elements';
 
 require('../css/application.css.scss');
 
-export { FormBuilder, FormGenerator, FormElement };
+export { FormBuilder, FormGenerator, FormElement, FormElements };
