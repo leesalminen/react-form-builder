28924f08f6f82679d4667554f1add2378bc19be2
Ian Zhang <ian@gingrapp.com>
2015-11-24 18:08:31 -0700

    Exposing FormElement

--- src/app.jsx
+++ src/app.jsx
@@ -2,7 +2,8 @@ import 'babel-polyfill';
 
 import FormBuilder from './form-builder';
 import FormGenerator from './form-generator';
+import FormElement from './form-elements/util/form-element.jsx';
 
 require('../css/application.css.scss');
 
-export { FormBuilder, FormGenerator };
+export { FormBuilder, FormGenerator, FormElement };
