4b7ef2d71b170dad58d51cd013b7d658e9808855
Ian Zhang <ian@gingrapp.com>
2015-11-24 18:33:57 -0700

    Exporting header bar and header label

--- src/app.jsx
+++ src/app.jsx
@@ -2,9 +2,11 @@ import 'babel-polyfill';
 
 import FormBuilder from './form-builder';
 import FormGenerator from './form-generator';
-import FormElement from './form-elements/util/form-element.jsx';
+import FormElement from './form-elements/util/form-element';
+import HeaderBar from './form-elements/util/header-bar'
+import HeaderLabels from './form-elements/util/header-labels'
 import * as FormElements from './form-elements';
 
 require('../css/application.css.scss');
 
-export { FormBuilder, FormGenerator, FormElement, FormElements };
+export { FormBuilder, FormGenerator, FormElement, FormElements, HeaderBar, HeaderLabels };
