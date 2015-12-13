a4fadd44040d548930b8ca05029b3de417e0de6c
Lee Salminen <lee@gingrapp.com>
2015-12-13 09:29:36 -0700

    Use props for tags
    I'd like to be able to add to Tags, Gingr style but not  mess up this repo.

--- src/form-builder-preview.jsx
+++ src/form-builder-preview.jsx
@@ -149,7 +149,7 @@ export default class FormBuilderPreview extends React.Component {
                 element             = {this.props.editElement}
                 updateElement       = {this.updateElement}
                 isSuperUser         = {this.props.isSuperUser}
-                tags                = {['Details', 'Main']} />
+                tags                = {this.props.tags} />
           }
         </div>
         <Sortable sensitivity={0} key={this.state.data.length} onSort={this.handleSort.bind(this)}>
