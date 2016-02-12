2e89f493c7f060b022e13ae2fb546033e69df7cb
Ian Zhang <ian@gingrapp.com>
2016-02-12 16:56:03 -0700

    Exporting FormElementStatic and FormElementWithOptions

--- src/app.jsx
+++ src/app.jsx
@@ -3,10 +3,12 @@ import 'babel-polyfill';
 import FormBuilder from './form-builder';
 import FormGenerator from './form-generator';
 import FormElement from './form-elements/util/form-element';
+import FormElementWithOptions from './form-elements/util/form-element-with-options';
+import FormElementStatic from './form-elements/util/form-element-static';
 import HeaderBar from './form-elements/util/header-bar'
 import HeaderLabels from './form-elements/util/header-labels'
 import * as FormElements from './form-elements';
 
 require('../css/application.css.scss');
 
-export { FormBuilder, FormGenerator, FormElement, FormElements, HeaderBar, HeaderLabels };
+export { FormBuilder, FormGenerator, FormElement, FormElementWithOptions, FormElementStatic, FormElements, HeaderBar, HeaderLabels };
