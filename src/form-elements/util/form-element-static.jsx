d06f92338f2f21d396560874121fd92e1c396b5a
Lee Salminen <lee@gingrapp.com>
2016-02-16 12:18:05 -0700

    adding a label to static fields

--- src/form-elements/util/form-element-static.jsx
+++ src/form-elements/util/form-element-static.jsx
@@ -1,6 +1,13 @@
 import FormElement from './form-element';
 
 export default class FormElementStatic extends FormElement {
+
+	static defaultOptions() {
+        return {
+            label: ''
+        }
+    }
+
     renderReadOnly() {
         return this.renderComponent();
     }
